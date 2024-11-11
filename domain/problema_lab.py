21cce5bf91ac669e5067ca57871bfaed7ecd0de6
Zdroba Petru <petru.zdroba@gmail.com>
2024-11-11 18:32:16 +0200

    added problema laborator class

---
+++ domain/problema_lab.py
@@ -0,0 +1,20 @@
+class Problema_Laborator(object):
+    def __intit__(self, laborator_numar: int, descriere: str, deadline: str):
+        self.__laborator_numar = laborator_numar
+        self.__descriere = descriere
+        self.__deadline = deadline
+
+    def set_descriere(self, descriere_noua: str):
+        self.__descriere = descriere_noua
+
+    def set_deadline(self, deadline_nou: str):
+        self.__deadline = deadline_nou
+
+    def get_laborator_numar(self):
+        return self.__laborator_numar
+
+    def get_descriere(self):
+        return self.__descriere
+
+    def get_dead__deadline(self):
+        return self.__deadline
