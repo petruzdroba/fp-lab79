440c5d837ab4a3b0062464bf8abbc54b20fb0d9b
Zdroba Petru <petru.zdroba@gmail.com>
2024-11-11 19:14:01 +0200

    added add student to student list

--- ui/menu.py
+++ ui/menu.py
@@ -1,3 +1,7 @@
+from service.student_service import Student_Service
+from domain.student import Student
+
+
 def meniu_consola(student_list: list):
     print("1. Adauga student")
     choice = int(input(">>>"))
@@ -7,4 +11,8 @@ def meniu_consola(student_list: list):
         nume = input("nume>>>")
         grupa = int(input("grupa>>>"))
 
-        pass
+        student = Student(student_id, nume, grupa)
+
+        service = Student_Service()
+
+        service.add_student_to_list(student_list, student)
