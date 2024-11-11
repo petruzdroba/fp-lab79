b50a5d2c38d9439eec3876f557b9897d6889cd4a
Zdroba Petru <petru.zdroba@gmail.com>
2024-11-11 19:38:52 +0200

    added test for adding problem

---
+++ service/probleme_service.py
@@ -0,0 +1,27 @@
+from domain.problema_lab import Problema_Laborator
+from domain.validators import Validator
+
+
+class Problema_Service(object):
+    def __init__(self) -> None:
+        pass
+
+    def add_problem_to_list(
+        self, list_of_questions: list, problema: Problema_Laborator
+    ):
+        """
+        Functie care adauga o problema la lista de probleme
+        import :
+            list_of_questions : list of obj
+            problem : obj
+        :returns : -
+        """
+        valideaza = Validator()
+
+        valideaza.valideaza_problema(
+            problema.get_laborator_numar(),
+            problema.get_descriere(),
+            problema.get_deadline(),
+        )
+
+        list_of_questions.append(problema)
