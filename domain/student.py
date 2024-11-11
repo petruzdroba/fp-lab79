0534d6d768577546c84b897d017db93bb5a4dd9b
Zdroba Petru <petru.zdroba@gmail.com>
2024-11-11 18:25:21 +0200

    defined class student and setters

---
+++ domain/student.py
@@ -0,0 +1,11 @@
+class Student(object):
+    def __init__(self, student_id: int, nume: str, grupa: int):
+        self.__student_id = student_id
+        self.__nume = nume
+        self.__grupa = grupa
+
+    def set_name(self, nume_nou: str):
+        self.__nume = nume_nou
+
+    def set_name(self, grupa: str):
+        self.__grupa = grupa
