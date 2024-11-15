1872d2c3a555160531779e3c5aabee353dced4a6
Zdroba Petru <petru.zdroba@gmail.com>
2024-11-15 20:55:02 +0200

    added student repo and add

--- repository/student_repo.py
+++ repository/student_repo.py
@@ -1,3 +1,13 @@
+from domain.student import Student
+
+
 class StudentRepo(object):
     def __init__(self):
         self.__student_list = {}
+
+    def get_student_list(self):
+        return self.__student_list
+
+    def add_student(self, student: Student):
+        id_student = student.get_id_student()
+        self.__studenti[id_student] = student
--- service/student_service.py
+++ service/student_service.py
@@ -1,10 +1,12 @@
 from validators.student_validator import StudentValidator
 from domain.student import Student
+from repository.student_repo import StudentRepo
 
 
 class Student_Service(object):
     def __init__(self):
         self.__valideaza_student = StudentValidator
+        self.__student_repo = StudentRepo
 
     def add_student_to_list(self, student_list: list, student: Student):
         """
@@ -13,24 +15,26 @@ class Student_Service(object):
             student_list : list
             student: student obj
         """
-        self.__valideaza_student.valideaza_student(student, student_list)
+        self.__valideaza_student.valideaza_student(
+            student, self.__student_repo.get_student_list()
+        )
 
-        student_list.append(student)
+        self.__student_repo.add_student(student)
 
-    def delete_student_from_list(self, student_list: list, student_id: int):
-        """
-        Functie care sterge un student dintr-o lista
-        input:
-            student_id : int
-            student_list : list
-        :return -
-            raise ValueError cu mesajul "student inexistent"
-        """
-        found = False
-        for student in student_list:
-            if student.get_id() == student_id:
-                student_list.pop(student_list.index(student))
-                found = True
-                break
-        if not found:
-            raise ValueError("student inexistent")
+    # def delete_student_from_list(self, student_list: list, student_id: int):
+    #     """
+    #     Functie care sterge un student dintr-o lista
+    #     input:
+    #         student_id : int
+    #         student_list : list
+    #     :return -
+    #         raise ValueError cu mesajul "student inexistent"
+    #     """
+    #     found = False
+    #     for student in student_list:
+    #         if student.get_id() == student_id:
+    #             student_list.pop(student_list.index(student))
+    #             found = True
+    #             break
+    #     if not found:
+    #         raise ValueError("student inexistent")
